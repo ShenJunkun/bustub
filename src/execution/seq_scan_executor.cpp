86fa2220cb1d68d4a91eca2956d69d9ed54cae6a
shenjunkun <2458684728@qq.com>
2022-12-20 10:36:46 +0800

    finish project3 seq_scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,52 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "include/storage/table/table_iterator.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), 
+                    cursor_(nullptr, RID(INVALID_PAGE_ID, 0), nullptr),
+                    end_(nullptr, RID(INVALID_PAGE_ID, 0), nullptr) {
+  this->plan_ = plan;
+}
 
-void SeqScanExecutor::Init() {}
+void SeqScanExecutor::Init() {
+  auto tableOid = plan_->GetTableOid();
+  auto catalog = this->GetExecutorContext()->GetCatalog();
+  auto tableInfo = catalog->GetTable(tableOid);
+  auto tableHeap = tableInfo->table_.get();
+  auto txn = this->GetExecutorContext()->GetTransaction();
+  cursor_ = tableHeap->Begin(txn);
+  end_ = tableHeap->End();
+}
 
-bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool SeqScanExecutor::Next(Tuple *tuple, RID *rid) {
 
+  if (cursor_ == end_) {
+    return false;
+  }
+
+  bool res = this->plan_->GetPredicate()->Evaluate(&(*cursor_), GetOutputSchema()).GetAs<bool>();
+  
+
+  while (!res)
+  {
+    cursor_++;
+    if (cursor_ == end_) {
+      break;
+    }
+    res = this->plan_->GetPredicate()->Evaluate(&(*cursor_), GetOutputSchema()).GetAs<bool>();
+  }
+
+  if (cursor_ != end_) {
+    *tuple = *cursor_;
+    *rid = (*cursor_).GetRid();
+    cursor_++;
+    return true;
+  } 
+  
+  return false; 
+}
+    
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -50,5 +50,7 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+  TableIterator cursor_;
+  TableIterator end_;
 };
 }  // namespace bustub
