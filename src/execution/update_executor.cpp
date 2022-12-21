407d87c3ad67adbb922eee12438d575ca7c9a168
JunkunShen <junkunshen@qq.com>
2022-12-21 23:17:09 +0800

    finish Project3 update executor

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -12,16 +12,46 @@
 #include <memory>
 
 #include "execution/executors/update_executor.h"
+#include "common/logger.h"
 
 namespace bustub {
 
 UpdateExecutor::UpdateExecutor(ExecutorContext *exec_ctx, const UpdatePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),  child_executor_(std::move(child_executor)) {
+  this->plan_ = plan;
+}
 
-void UpdateExecutor::Init() {}
+void UpdateExecutor::Init() {
+  auto talbeOid = this->plan_->TableOid();
+  this->table_info_ = this->GetExecutorContext()->GetCatalog()->GetTable(talbeOid);
+  child_executor_.get()->Init();
+}
 
-bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+
+  bool res =child_executor_.get()->Next(tuple, rid);
+  if (!res) {
+    return false;
+  }
+
+  Tuple newTuple = this->GenerateUpdatedTuple(*tuple);
+
+  bool updateRes = table_info_->table_.get()->UpdateTuple(newTuple, *rid, exec_ctx_->GetTransaction());
+  if (!updateRes) {
+    LOG_WARN("update executor更新失败");
+  }
+
+  //update indexes
+  std::vector<IndexInfo *> indexes = this->GetExecutorContext()->GetCatalog()->GetTableIndexes(table_info_->name_);
+  for (auto index : indexes) {
+    index->index_.get()->DeleteEntry(*tuple, *rid, exec_ctx_->GetTransaction());
+    index->index_.get()->InsertEntry(newTuple, *rid, exec_ctx_->GetTransaction());
+  }
+
+  
+   return true; 
+}
 
 Tuple UpdateExecutor::GenerateUpdatedTuple(const Tuple &src_tuple) {
   const auto &update_attrs = plan_->GetUpdateAttr();
