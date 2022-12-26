dc0aa9d7de776f99d05a0c8b3f9064fffd82ebe6
JunkunShen <junkunshen@qq.com>
2022-12-26 16:05:20 +0800

    finish Project3 limit_executor

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -16,10 +16,26 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+  : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)), count_(0) {}
 
-void LimitExecutor::Init() {}
+void LimitExecutor::Init() {
+  child_executor_.get()->Init();
+}
 
-bool LimitExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
+  Tuple child_tuple;
+  RID child_rid;
+  if (count_ >= plan_->GetLimit()) {
+    return false;
+  }
+
+  auto res = child_executor_.get()->Next(&child_tuple, &child_rid);
+  if (!res) {
+    return false;
+  }
+  *tuple = child_tuple;
+  count_++;
+  return true; 
+}
 
 }  // namespace bustub
